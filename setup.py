8672c8e5b7b2dac65562bcc09a94de67ad14c30b
VenkateshL1921 <venkyslotlikar19@gmail.com>
2024-01-11 15:23:24 +0530

    project setup completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Mental Health Chatbot',
+    version= '0.0.0',
+    author= 'Venkatesh Lotlikar',
+    author_email= 'venkyslotlikar19@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
